6c47258dd72499a67f12e962d835d85c51152d5d
longlong <562172151@qq.com>
2020-03-08 13:39:54 +0800

    u > src/components -->(input-row)[新增了,value,icon,readonly,placeholder,label,lableflex,inplueflex 等属性]

--- lab/routers/form.vue
+++ lab/routers/form.vue
@@ -1,25 +1,78 @@
 <template>
   <div>
     <lg-input-group>
-      <lg-input-row></lg-input-row>
+      <lg-input-row icon="" v-model="value"  @click="handleClick()"></lg-input-row>
+      <lg-input-row icon="select" :label-flex="2" :input-flex="2" placeholder="placeholder" readonly></lg-input-row>
     </lg-input-group>
     <hr />
     <lg-table :data="datas"></lg-table>
   </div>
 </template>
 <script>
+
 export default {
   data() {
     return {
+      value:"",
       datas: [
         {
-          property: "-",
-          declare: "-",
-          mold: "-",
-          default: "-"
-        }
+          property: "label(row)",
+          declare: "input的标签",
+          mold: "String",
+          default: "默认"
+        },
+        {
+          property: "icon(row)",
+          declare: "图标",
+          mold: "String",
+          default: "--"
+        },
+        {
+          property: "placeholder(row)",
+          declare: "input的原生属性placeholder",
+          mold: "String",
+          default: "请输入"
+        },
+        {
+          property: "labelFlex(row)",
+          declare: "标签的flex",
+          mold: "Number",
+          default: "1"
+        },
+        {
+          property: "inputFlex(row)",
+          declare: "input的flex",
+          mold: "Number",
+          default: "1"
+        },
+        {
+          property: "readonly(row)",
+          declare: "input的原生属性readonly",
+          mold: "Boolean",
+          default: "false"
+        },
+        {
+          property: "readonly(row)",
+          declare: "input的原生属性readonly",
+          mold: "Boolean",
+          default: "false"
+        },
+        {
+          property: "value(row)",
+          declare: "input的输入值",
+          mold: "String",
+          default: "--"
+        },
       ]
     };
-  }
+  },
+  mounted() {
+    console.log(this.value);
+  },
+  methods: {
+    handleClick(){
+      window.alert("触发了~"+this.value)
+    }
+  },
 };
 </script>
--- src/components/form/input-row.vue
+++ src/components/form/input-row.vue
@@ -1,33 +1,113 @@
 <template>
   <div :class="classes" :style="styles">
-     <label :class="labelClasses"></label>
-     <input :class="inputClasses" type="text" >
+    <div :class="labelWrapClasses" :style="labelStyles">
+      <label :class="labelClasses" v-text="label" ></label>
+    </div>
+    <div :class="inputWrapClasses" :style="inputStyles">
+      <input :class="inputClasses" :value="currentValue" @input="handleInput" type="text" :placeholder="placeholder" :readonly="readonly" />
+    </div>
+    <i :class="iconClasses" @click="handleClick" v-if="icon"></i>
   </div>
 </template>
 <script>
 const prefixCls = "lg-input-row";
+const iconPrefixCls = "lg-icon";
 export default {
   name: "LInputRow",
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {
-    classes() {
-        return[prefixCls,{} ] 
+  props: {
+    label:{
+      type:String,
+      default:"默认"
     },
-    labelClasses() {
-        return[`${prefixCls}-label`] 
+    icon: {
+      type: String
     },
-    inputClasses() {
-        return[`${prefixCls}-input`] 
+    placeholder:{
+      type:String,
+      default:"请输入"
     },
-    styles() {
-       let style = {}
-
-       return style
+    labelFlex: {
+      type: Number
+    },
+    inputFlex: {
+      type: Number
+    },
+    readonly:{
+      type:Boolean
+    },
+    value:{
+      type:String
     }
   },
-  methods: {}
+  data() {
+    return {
+      currentValue:""
+    };
+  },
+
+  computed: {
+    classes() {
+      return [prefixCls, {}];
+    },
+    labelClasses() {
+      return [`${prefixCls}-label`];
+    },
+    labelWrapClasses() {
+      return [`${prefixCls}-label-wrap`];
+    },
+    inputClasses() {
+      return [`${prefixCls}-input`];
+    },
+    inputWrapClasses() {
+      return [`${prefixCls}-input-wrap`];
+    },
+    iconClasses() {
+      return [
+        `${iconPrefixCls}`,
+        `${prefixCls}-icon`,
+        {[`${iconPrefixCls}-${this.icon}`]:!!this.icon},
+        {[`${iconPrefixCls}-ios-arrow-forward`]:this.icon === 'select'},
+      ];
+    },
+    styles() {
+      let style = {};
+
+      return style;
+    },
+    labelStyles() {
+      let style = {};
+      if(this.labelFlex) style['flex'] = this.labelFlex;
+      return style;
+    },
+    inputStyles() {
+      let style = {};
+      if(this.inputFlex) style['flex'] = this.inputFlex;
+      return style;
+    }
+  },
+  watch: {
+    value(val){
+        if(val){
+          this.currentValue = this.value; 
+        }
+    },
+    currentValue(val){
+        if(val){
+            this.value = this.currentValue; 
+        }
+    }
+  },
+  mounted() {
+    console.log(">>>>>"+this.icon)
+  },
+  methods: {
+    handleInput(){
+       let val = event.target.value;
+       if(val) this.$emit('input',val); 
+    },
+    handleClick() {
+      this.$emit("click");
+    }
+  }
 };
 </script>
